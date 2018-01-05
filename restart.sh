fdd5fad76ab13765efb20363d9c89680af0a1a9b
cryptonew <cryptonew2017@gmail.com>
2018-01-05 21:53:21 +0200

    changed restart

--- restart.sh
+++ restart.sh
@@ -2,6 +2,6 @@
 # This script restarts python nicebot process
 
 ps aux | grep -v grep | grep nicebot.py | awk '{ print $2 }' | xargs kill -9
-python ~centos/nicebot/nicebot.py >> ~centos/nicebot/bot.log 2>&1 &
+python ~centos/nicetelebot/nicebot.py >> ~centos/nicetelebot/bot.log 2>&1 &
 
 
